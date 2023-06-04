c01b125cee861fdf4881d145e3b62b8905396d42
“Martin <mporcheron@users.noreply.github.com>
2023-06-04 17:01:56 +0100

    Update BiBTex template

--- assets/js/copyclose.js
+++ assets/js/copyclose.js
@@ -1,33 +1,22 @@
-$(function() {
-    $('.nojs-none').removeClass('nojs-none');
-    if (window.opener) {
-        $('.noparentwin-none').removeClass('noparentwin-none');
-    } else {
-        $('.parentwin-none').removeClass('parentwin-none');
-    }
-    
-    // Copy buttons
-    if($('button[data-copy=true]').length > 0) {
-        $.getScript('/assets/js/clipboard.min.js', function() {
-            var clipboard = new ClipboardJS('[data-copy="true"]', {
-                target: function(trigger) {
-                    return document.getElementById($(trigger).data('target').substring(1));
-                }
-            });
+window.addEventListener('load', () => {
+	var clipboard = new ClipboardJS('[data-copy="true"]', {
+		target: function(trigger) {
+			return document.getElementById(trigger.dataset.target.substring(1));
+		}
+	});
 
-            clipboard.on('success', function(e) {
-                e.clearSelection();
-            });
+	clipboard.on('success', function(e) {
+		e.clearSelection();
+	});
 
-            clipboard.on('error', function(e) {
-                alert('Sorry, there was an error adding the BibTeX to your clipboard. Please try again manually by selecting the BibTeX source and using the copy function on your device.')
-            });
-        });
-    }
+	clipboard.on('error', function(e) {
+		alert('Sorry, there was an error adding the BibTeX to your clipboard. Please try again manually by selecting the BibTeX source and using the copy function on your device.')
+	});
     
     // Close buttons
-    $('button[data-close=true]').click(function() {
-        window.close()
+    document.querySelectorAll('button[data-close=true]').forEach((button) => {
+        button.addEventListener('click', event => {
+			window.close()
+        });
     });
-
 });
