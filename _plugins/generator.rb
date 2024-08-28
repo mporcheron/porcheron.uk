662e357636162a8d693ff544abaec0183a673b59
Martin Porcheron <mporcheron@users.noreply.github.com>
2024-08-28 18:25:58 +0000

    Refactor code

--- _plugins/generator.rb
+++ _plugins/generator.rb
@@ -10,38 +10,48 @@ module JekyllLastCommit
       @repo_man = JekyllLastCommit::RepoMan.new(site.source)
       @repo_man.discover_repo()
 
-      data_file_extensions = ['.yml', '.yaml', '.json', '.tsv', '.csv']
-      data_files = []
-      site.data.keys.each do |data_file|
-        data_file_extensions.each do |data_file_extension|
-          data_file_name = data_file + data_file_extension
-          if File.file?('./_data/' + data_file_name)
-            data_files.append(data_file_name)
-            break
-          end
-        end
-      end
-
-      @repo_man.discover_commits(site.documents.map {|d| d.relative_path })
-      @repo_man.discover_commits(site.pages.map {|p| p.relative_path })
-      @repo_man.discover_commits(site.static_files.map {|sf| '.' + sf.relative_path })
-      @repo_man.discover_commits(data_files.map {|df| './_data/' + df })
-
       @date_format = site.config.dig('jekyll-last-commit', 'date_format')
       @date_format ||= '%B %d, %Y'
 
       @should_fall_back_to_mtime = site.config.dig('jekyll-last-commit', 'should_fall_back_to_mtime')
       @should_fall_back_to_mtime ||= true
 
-      data_files_key = site.config.dig('jekyll-last-commit', 'data_files_key')
-      data_files_key ||= 'meta'
-
+      @repo_man.discover_commits(site.documents.map {|d| d.relative_path })
       populate_jekyll(site.documents)
-      populate_jekyll(site.pages)
-      populate_jekyll(site.static_files, '.')
 
-      site.data[data_files_key] = {}
-      populate_data(data_files, site.data[data_files_key], './_data/')
+      @repo_man.discover_commits(site.pages.map {|p| p.relative_path })
+      populate_jekyll(site.pages)
+
+      index_static_files = site.config.dig('jekyll-last-commit', 'index_static_files')
+      index_static_files ||= false
+      if index_static_files
+        @repo_man.discover_commits(site.static_files.map {|sf| '.' + sf.relative_path })
+        populate_jekyll(site.static_files, '.')
+      end
+
+      index_data_files = site.config.dig('jekyll-last-commit', 'index_data_files')
+      index_data_files ||= false
+      if index_data_files
+        data_file_extensions = ['.yml', '.yaml', '.json', '.tsv', '.csv']
+        data_files = []
+        site.data.keys.each do |data_file|
+          data_file_extensions.each do |data_file_extension|
+            data_file_name = data_file + data_file_extension
+            if File.file?('./_data/' + data_file_name)
+              data_files.append(data_file_name)
+              break
+            end
+          end
+        end
+      
+        data_files_key = site.config.dig('jekyll-last-commit', 'data_files_key')
+        data_files_key ||= 'meta'
+
+        @repo_man.discover_commits(data_files.map {|df| './_data/' + df })
+
+        site.data[data_files_key] = {}
+        populate_data(data_files, site.data[data_files_key], './_data/')
+      end
     end
 
     def populate_jekyll(files, prepend_path = '')
@@ -67,8 +77,6 @@ module JekyllLastCommit
           file.data['last_modified_at_formatted'] = raw_time.strftime(@date_format)
         end
       end
-
-      return files
     end
 
     def populate_data(source, output = {}, prepend_path = '')
